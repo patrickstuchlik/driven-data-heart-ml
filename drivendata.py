b172f3c8d934d4c39f415f72cb21fdb0a7d273a9
Patrick Stuchlik <pat.stuchlik@gmail.com>
2020-05-26 17:04:34 -0400

    adding UCI ML data

--- drivendata.py
+++ drivendata.py
@@ -13,4 +13,13 @@ from sklearn.linear_model import SGDClassifier
 est = [('ss',StandardScaler()),('sgd',SGDClassifier())]
 pipe = Pipeline(est)
 
+le = sklearn.preprocessing.LabelEncoder()
+le.fit(train_x['thal'])
+list(le.classes_)
+train_x['thal2'] = le.transform(train_x['thal'])
+train_x.head()
+
+# clf = SGDClassifier()
+# clf.fit(train_x,train_y)
+#%%
 #pipe.fit(train_x.iloc[:,1:],train_y)

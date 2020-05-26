b451bdaa6f830414c4f9c9c8b6803c2abc066bfe
Patrick Stuchlik <pat.stuchlik@gmail.com>
2020-05-26 13:19:04 -0400

    import csv

--- drivendata.py
+++ drivendata.py
@@ -1,3 +1,4 @@
 import pandas as pd
-train = pd.read_csv('train_values.csv')
-print(train.head())
+train_x = pd.read_csv('train_values.csv')
+train_y = pd.read_csv('train_labels.csv')
+test_x = pd.read_csv('test_values.csv')

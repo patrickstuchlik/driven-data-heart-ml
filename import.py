a1e69cd495470789ca0431f27834d99914fa7335
Patrick Stuchlik <pat.stuchlik@gmail.com>
2020-05-26 12:12:53 -0400

    fixed import column titles

--- import.py
+++ import.py
@@ -1,5 +1,5 @@
 import pandas as pd
 url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/statlog/heart/heart.dat'
 
-heart = pd.read_fwf(url)
-heart.head()
+heart = pd.read_csv(url,sep='\s+', names = ['age','sex','pain','bp','cholesterol','sugar','ecg','hr','angina','oldpeak','slope','vess','thal','disease'])
+print(heart.head())

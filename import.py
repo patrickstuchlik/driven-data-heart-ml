4b65eb40ce969ed9434669d44318844982617fae
Patrick Stuchlik <pat.stuchlik@gmail.com>
2020-05-26 12:44:04 -0400

    adding test and train data

--- import.py
+++ import.py
@@ -2,4 +2,7 @@ import pandas as pd
 url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/statlog/heart/heart.dat'
 
 heart = pd.read_csv(url,sep='\s+', names = ['age','sex','pain','bp','cholesterol','sugar','ecg','hr','angina','oldpeak','slope','vess','thal','disease'])
+
+heart[['age','sex','pain','sugar','ecg','angina','slope','vess','thal']] = heart[['age','sex','pain','sugar','ecg','angina','slope','vess','thal']].astype(int)
+
 print(heart.head())
